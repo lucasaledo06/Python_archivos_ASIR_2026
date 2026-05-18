3b420709f0decab8eeb714a8aec4238e47c96ef1
Lucas Salas Ledo <152426317+lucasaledo06@users.noreply.github.com>
2026-05-18 09:42:22 +0200

    Update Actividad3.py

--- Actividad3.py
+++ Actividad3.py
@@ -3,9 +3,7 @@ RECARGO_EXPRESS = 15.0
 
 
 def procesar_pedido(item, cantidad, descuento, envio_express=False):
-    total = PRECIO_BASE * cantidad
-
-    total = total - (total * descuento)
+    total = (PRECIO_BASE * cantidad) * (1 - descuento)
 
     if envio_express:
         total += RECARGO_EXPRESS
@@ -14,34 +12,22 @@ def procesar_pedido(item, cantidad, descuento, envio_express=False):
 
 
 print("---- Procesador de Pedidos en Linea ----")
-print("Precio base del artículo: 50.00 €")
 
 item = input("1. Introduce el nombre del articulo: ")
 cantidad = int(input("2. Introduce la cantidad de unidades: "))
-descuento_input = float(input("3. Introduce el porcentaje de descuento (ej: 10 para 10%): "))
-envio = input("4. ¿Desea envío Express (S/N)? ").upper()
+descuento = float(input("3. Descuento (ej: 10 para 10%): ")) / 100
+envio_express = input("4. ¿Envio Express (S/N)? ").upper() == "S"
 
-# convertir datos
-descuento = descuento_input / 100
-
-if envio == "S":
-    envio = True
-else:
-    envio = False
-
-
-# llamada con parámetros posicionales + por nombre
 total = procesar_pedido(
     item,
     cantidad,
     descuento=descuento,
-    envio_express=envio
+    envio_express=envio_express
 )
 
 print("\n---- Resultado del Pedido ----")
 print(f"Artículo: {item}")
 print(f"Unidades: {cantidad}")
-print(f"Descuento: {descuento_input}%")
-print(f"Envío Express: {'Sí' if envio else 'No (Default: No)'}")
-
-print(f"\n💰 El COSTO TOTAL del pedido es: € **{total:.2f}**")
+print(f"Descuento: {descuento*100:.0f}%")
+print(f"Envío Express: {'Sí' if envio_express else 'No'}")
+print(f"\nTOTAL: € {total:.2f}")
